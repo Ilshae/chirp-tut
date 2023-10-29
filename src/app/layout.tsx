09b1b33d5edb372503de29f5e8991960c31b5e49
Ilshae <pruszynskaingrid@outlook.com>
2023-10-29 12:10:29 +0100

    add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { headers } from "next/headers";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -24,7 +25,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+        <ClerkProvider>
+          <TRPCReactProvider headers={headers()}>
+            <UserButton afterSignOutUrl="/" />
+            {children}
+          </TRPCReactProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
